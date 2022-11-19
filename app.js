7af5d938099f5b69af707f640ae187789fad70c8
Ashley Wyatt <67844566+ashleywyattcodes@users.noreply.github.com>
2022-11-19 14:12:14 -0600

    Update app.js & index.html
    Created function that will not let user select Submit unless they have actually clicked a number.

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const submitButton = document.getElementById("submit")
 const rateAgain = document.getElementById("rate-again")
 const rating = document.getElementById("rating")
 const rates = document.querySelectorAll(".btn")
-submitButton.addEventListener("click", () => {
-   thanksContainer.classList.remove("hidden")
-   popUpBox.style.display = "none"
-})
 rateAgain.addEventListener("click", () => {
    thanksContainer.classList.add("hidden")
    popUpBox.style.display = "block"
@@ -15,5 +11,9 @@ rateAgain.addEventListener("click", () => {
 rates.forEach((rate) => {
    rate.addEventListener("click", () => {
       rating.innerHTML = rate.innerHTML
+      submitButton.addEventListener("click", () => {
+         thanksContainer.classList.remove("hidden")
+         popUpBox.style.display = "none"
+      })
    })
 })
